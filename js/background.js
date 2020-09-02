9e8109c0afda29c1aa24d3058155db1cd35a230a
JimSunJing <jimsun6428@gmail.com>
2020-09-02 23:54:04 +0800

    crawler script 9-2

--- js/background.js
+++ js/background.js
@@ -1,43 +1,138 @@
 console.log("background.js is running...")
 
+const UserKey = (douId) => {
+    return "douban_user_" + douId
+}
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log(message)
-    // 下面应该开始爬虫
-    mainControl(message.douId)
+    if (sender.url.indexOf("www.douban.com/people") !== -1) {
+        // 下面应该开始爬虫
+        const Ukey = UserKey(message.douId)
+        chrome.storage.local.set({ Ukey: message }, () => {
+            console.log("user info saved", message)
+        })
+        movieMainControl(message)
+    }
+    sendResponse("background got the message..")
 })
 
 // 爬虫部分
 // 主控制程序，翻页之类的
-const mainControl = (douId) => {
-    const url = "https://movie.douban.com/people/"+ douId + "/collect?sort=time&amp;start=0&amp;filter=all&amp;mode=list&amp;tags_sort=count"
-    savePage(url, douId)
-    
-    
+const movieMainControl = (userInfo) => {
+    // 电影看过爬取
+    let page = 0
+    const url = "https://movieItem.douban.com/people/" + userInfo.douId +
+        "/collect?sort=time&amp;start=" + page * 30 +
+        "&amp;filter=all&amp;mode=list&amp;tags_sort=count"
+    if (!saveMoviePage(url, userInfo)) {
+        // 已有重复爬取条目，可退出
+        return false
+    }
+
+
 }
-
-// 从items获取信息，存入chrome.storage
-const saveItem = (item, douId) => {
-
-}
-
-// 进入subject页面进行信息收集，存入 chrome.storage
-
-// 
-
-// 获取dom
-const savePage = (url, douId) => {
-    fetchText(url).then((text) => {
-        console.log(text)
-        let dom =  new DOMParser().parseFromString(text, "text/html");
-        // 获取 items 元素
-        let items = dom.querySelectorAll(".item")
-        console.log(items[0])
-    }).catch(e => {
-        console.log("error when fetching:",url,"info:",e);
-    })
-}
-
+// 异步请求页面
 const fetchText = async (url) => {
     let response = await fetch(url)
     return await response.text()
 }
+
+// 获取storage内的movie id
+const getUserMovies = (douId) => {
+    chrome.storage.local.get("user_" + douId, (data) => {
+        // data["movies"] 应该是 Array
+        return data["movies"] === null ? null :
+            data["movies"].map((row) => { row["subjectId"] })
+    })
+}
+
+// 获取dom
+const saveMoviePage = (url, userInfo) => {
+    fetchText(url)
+    .then((text) => {
+        // 标记是否已经爬到历史记录
+        let finished = false
+        // console.log(text)
+        let dom = new DOMParser().parseFromString(text, "text/html");
+        // 先获取所有已经保存好的电影条目ID，避免多次爬取
+        const movieIds = getUserMovies(userInfo.douId)
+        // 获取 items 元素
+        let items = Array.from(dom.querySelectorAll(".item"))
+        let movieMap = Map()
+        for (let i = 0; i<= items.length; i++){
+            let movie = getMovieItem(item, userInfo.douId, movieIds)
+            if (movie === false) {
+                // 意味着有重复爬取的条目了，标记一下
+                finished = true
+                break
+            }else {
+                // 有点怪怪的。。。2020年9月2日23点52分睡觉
+                movieMap.set(movie.subjectId, movieItem)
+            }
+        }
+    }).catch(e => {
+        console.log("error when fetching:", url, "info:", e);
+    })
+}
+
+
+// 从items获取信息，存入chrome.storage
+const getMovieItem = (item, douId, movieIds) => {
+    const subjectId = item.querySelector("a").href.split("/")[4]
+    if (movieIds.findIndex((x) => { x === subjectId }) !== -1) {
+        return false
+    }
+    let movieItem = {
+        "subjectId": subjectId,
+        "电影名": item.querySelector(".title a").innerText,
+        "豆瓣链接": item.querySelector("a").href,
+        "我的评分": item.querySelector(".date span").className.charat(6),
+        "标记时间": item.querySelector(".date").innerText.trim(),
+        "短评": items[4].querySelector(".comment") !== null ? 
+            items[4].querySelector(".comment").innerText.trim() : "..."
+    }
+    console.log("simple:",movieItem)
+    // 详细信息收集
+    const details = movieDetail(subjectId)
+    movieItem = {
+        ...movieItem,
+        ...details
+    }
+    // 返回一个movie对象
+    return movieItem
+}
+
+// 进入subject页面进行信息收集，存入 chrome.storage
+const movieDetail = (movieSubjectId) => {
+    // 拼接url
+    const url = 'https://movieItem.douban.com/subject/' + movieSubjectId
+    fetchText(url)
+    .then((text) => {
+        const dom = new DOMParser().parseFromString(text, "text/html");
+        let details = {
+            "年份": dom.querySelector(".year").innerText.replace("(","").replace(")",""),
+            "封面": dom.querySelector("#mainpic img").src.replace("webp","jpg")
+        }
+        const infos = Array.from(dom.querySelector("#info").innerText.split(/\r?\n/))
+        infos.reduce((acc, curr)=>{
+            const k = curr.split(": ")[0]
+            const v = curr.split(": ")[1]
+            return {
+                ...acc,
+                ...{[k]: v}
+            }
+        }
+        ,details)
+        console.log(details)
+        // 直接返回detail Object
+        return details
+    }).catch(e => {
+        console.log("error when fetching:", url, "info:", e);
+        return null
+    })
+}
+
+// 
+
+
--- js/cs.js
+++ js/cs.js
@@ -4,12 +4,42 @@ const doubanBtn = document.createElement('button')
 doubanBtn.className = 'NoBanBtn'
 doubanBtn.textContent = "备份电影到 NoBan"
 
+const ItemCount = (counts) => {
+    return Array.from(counts)
+    .map((x)=>{
+        let i = parseInt(x.innerText.split("部")[0])
+        if (x.innerText.indexOf("在") !== -1){
+            return {"ing": i}
+        }else if (x.innerText.indexOf("想") !== -1){
+            return {"wish": i}
+        }
+        return {"done": i}
+    })
+    .reduce((acc,curr)=>{
+        return {...acc, ...curr}
+    })
+}
+
 doubanBtn.onclick = () => {
     console.log("可以开始爬了")
-    const DouId = document.querySelector(".user-info .pl").innerHTML.split("<br>")[0].trim()
+    // 获取豆瓣用户的 ID
+    const DouId = document.querySelector(".user-info .pl")
+        .innerHTML.split("<br>")[0].trim()
     console.log("douban id:",DouId)
+    // 获取条目计数
+    // 电影
+    const movieCounts = ItemCount(document.querySelectorAll("#movie h2 span a"))
+    // 图书
+    const bookCounts = ItemCount(document.querySelectorAll("#book h2 span a"))
+    // 音乐 
+    const musicCounts = ItemCount(document.querySelectorAll("#music h2 span a"))
     // 接下来应该把 id 发给 background.js
-    chrome.runtime.sendMessage({douId: DouId}, (response) => {})
+    chrome.runtime.sendMessage({
+        douId: DouId, 
+        movieCounts: movieCounts,
+        bookCounts: bookCounts,
+        musicCounts: musicCounts
+    }, (response) => {console.log("response:",response)})
 }
 
 window.onload = () => {
